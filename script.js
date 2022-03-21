f56784fc97c554115935a1f713215a73bf41c28d
= <=>
2022-03-21 02:25:50 +0000

    JS additions

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate / 100;
+
+    var year = new Date().getFullYear() + parseInt(years);
+
+    var result = document.getElementById("result")
+
+    if (principal <= 0) {
+        alert("Ener a positive number");
+        return false;
+    }
     
+
+
+    content = `If you deposit ${principal}, \<br\>` +
+    `at an interest rate of ${rate}%. \<br\>` +
+    `You will recieve an amount of ${interest},\<br\>` +
+    `in the year ${year}.`;
+
+    result.innerHTML = content;
+
 }
+
+ function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+    }
         
